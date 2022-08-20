6c32423db6d52eeb6c7a37f2ea0f3bed4b975fff
Nitin Sharma <95009807+ignitinsharma@users.noreply.github.com>
2022-08-20 22:53:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 function addpost() {
 
     //--> taking vales to the user 
-    let id = document.getElementById("id").value;
-    let title = document.getElementById("title").value;
-    let author = document.getElementById("author").value;
+    let Id = document.getElementById("id").value;
+    let Title = document.getElementById("title").value;
+    let Author = document.getElementById("author").value;
 
     let send_data = {
-        id: id,
-        title: title,
-        author: author,
+        id: Id,
+        title: Title,
+        author: Author,
     };
 
+    //--> default fetch makes (GET--> get for get the data) request but 
+    //--> but this time we need to send data so we need (POST request) so we need to tell
+    //--> fetch fetch(URl, { methid:POST (so we are telling fetch we want that request) })
 
+    
     //--> for accessing the post we are using that post server
     let res=await fetch(`http://localhost:3000/posts`,{
 
+    //--> So we are telling fetch we want POST request(means send data request)
     method:'POST',
 
     body:JSON.stringify(send_data),
